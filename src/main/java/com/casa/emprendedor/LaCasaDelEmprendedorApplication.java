d4510321d4e637398f7e5773626da358a0d477bd
Gabriel Hernandez <gbhr115@gmail.com>
2022-01-31 21:58:40 -0300

    initial commit

---
+++ src/main/java/com/casa/emprendedor/LaCasaDelEmprendedorApplication.java
@@ -0,0 +1,13 @@
+package com.casa.emprendedor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaCasaDelEmprendedorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LaCasaDelEmprendedorApplication.class, args);
+	}
+
+}
