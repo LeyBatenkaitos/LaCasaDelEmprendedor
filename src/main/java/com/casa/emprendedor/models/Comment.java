3c45be887c411f615ca80b9786ad7fa17a225a19
RoblesGallardo97 <roblesgallardo1997@gmail.com>
2022-02-03 12:58:10 -0300

    {ER} Backend
    all models

--- src/main/java/com/casa/emprendedor/models/Comment.java
+++ src/main/java/com/casa/emprendedor/models/Comment.java
@@ -1,55 +1,44 @@
 package com.casa.emprendedor.models;
 
 import java.util.Date;
-import java.util.List;
-import javax.persistence.CascadeType;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+
 import org.springframework.format.annotation.DateTimeFormat;
+
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
-@Table(name="user")
+@Table(name="comment")
 @Getter
 @Setter
 @NoArgsConstructor
-public class User {
+public class Comment {
 	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	
 	private Long id;
 	@NotNull
-	private String firstname;
+	@Size(max=200)
+	private String content;
 	@NotNull
-	private String lastname;
+	private String username;
 	@NotNull
 	private String email;
-	@NotNull
-	@Size(min = 5, message = "Minimo 5 caracteres la contraseña")
-	private String password;
 	@Column(updatable = false)
 	@DateTimeFormat(pattern = "yyyy-MM-dd")
 	private Date createdAt;
 	@DateTimeFormat(pattern = "yyyy-MM-dd")
 	private Date updatedAt;
-	
-	@OneToMany(mappedBy="user", fetch = FetchType.LAZY)
-	private List<Bussines> bussines;
-	
-	@ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
-	@JoinColumn(name = "role_id")
-	private Role role;
 }
